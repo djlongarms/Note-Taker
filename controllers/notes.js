baaebc087a633abf9152fc055346651d82ef2c5f
Nick Palmer <ylnickpalmer@gmail.com>
2021-03-11 23:16:09 -0800

    finished code, removed test note, added README

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,17 +2,14 @@ const router = require('express').Router()
 const notes = require('../db/db.json')
 
 router.get('/notes', (req, res) => {
-  res.send({
-    status: 200,
-    message: 'GET Request successful',
-    data: notes
-  })
+  res.send(notes)
 })
 
 router.post('/notes', (req, res) => {
   let newNote = {
     title: req.body.title,
-    text: req.body.text
+    text: req.body.text,
+    id: notes.length + 1
   }
 
   notes.push(newNote)
@@ -21,7 +18,11 @@ router.post('/notes', (req, res) => {
 })
 
 router.delete('/notes/:id', (req, res) => {
-  notes.splice(req.params.id, 1)
+  notes.splice(req.params.id - 1, 1)
+
+  for(let i = req.params.id - 1; i < notes.length; i++) {
+    notes[i].id = i + 1;
+  }
 
   res.sendStatus(200)
 })
